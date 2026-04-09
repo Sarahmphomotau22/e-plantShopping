c34b25ab368e77cd6b7e4499cf23add999f3bb15
sarah mpho <sarahmphomotau22@gmail.com>
2026-04-09 21:07:17 +0200

    e

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,8 @@
+
 import React from 'react'
+import { Provider } from 'react-redux';
+import store from './store.js';
+
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
